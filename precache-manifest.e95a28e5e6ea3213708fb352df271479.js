38e2bd3e329bcc9a2dfcf1ddd57d181cc06e3ea5
Abhishek Ojha <ojha439@gmail.com>
2020-07-19 14:32:13 +0545

    Updates

---
+++ precache-manifest.e95a28e5e6ea3213708fb352df271479.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9378eaa2cc16ede8956b9f2d7109c196",
+    "url": "/react-breakingbad/index.html"
+  },
+  {
+    "revision": "394fbb7865ad70afd930",
+    "url": "/react-breakingbad/static/css/main.c5efc702.chunk.css"
+  },
+  {
+    "revision": "acb47738f96100399bfd",
+    "url": "/react-breakingbad/static/js/2.baaff3d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-breakingbad/static/js/2.baaff3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394fbb7865ad70afd930",
+    "url": "/react-breakingbad/static/js/main.0da22578.chunk.js"
+  },
+  {
+    "revision": "ad1dd6324ef03fceaeb4",
+    "url": "/react-breakingbad/static/js/runtime-main.e9b35198.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-breakingbad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-breakingbad/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-breakingbad/static/media/spinner.235b9b3e.gif"
+  }
+]);
